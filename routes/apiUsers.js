a8129cab0cacf383bdd3382e011af51caa7e9027
NicolasDubois68 <76472293+NicolasDubois68@users.noreply.github.com>
2020-12-29 11:18:01 +0100

    Update apiUsers.js

--- routes/apiUsers.js
+++ routes/apiUsers.js
@@ -6,19 +6,19 @@ const database = require('../database/users');
 function addApiRoutes(app, config, callback) {
     database(config, users  => {
         
-        app.get('/api/users', (req, res) => {
+        app.get('/apiUsers/users', (req, res) => {
             users.find(results => {
                 res.json({ data: results });
             });
         });
 
-        app.post('/api/users', (req, res) => {
+        app.post('/apiUsers/users', (req, res) => {
             users.add(req.body, id => {
                 res.json({ success: Boolean(id) });
             });
         });
 
-        app.all('/api/*', (req, res) => {
+        app.all('/apiUsers/*', (req, res) => {
             res.json({ error: 404, message: 'Unknown route' });
         });
         
