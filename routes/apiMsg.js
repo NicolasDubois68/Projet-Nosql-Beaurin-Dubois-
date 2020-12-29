a820498c2ae418620c6764760c2c35319212efaf
NicolasDubois68 <76472293+NicolasDubois68@users.noreply.github.com>
2020-12-29 11:17:28 +0100

    Update apiMsg.js

--- routes/apiMsg.js
+++ routes/apiMsg.js
@@ -6,19 +6,19 @@ const database = require('../database/msg');
 function addApiRoutes(app, config, callback) {
     database(config, msg  => {
         
-        app.get('/api/msg', (req, res) => {
+        app.get('/apiMsg/msg', (req, res) => {
             msg.find(results => {
                 res.json({ data: results });
             });
         });
 
-        app.post('/api/users', (req, res) => {
+        app.post('/apiMsg/msg', (req, res) => {
             msg.add(req.body, id => {
                 res.json({ success: Boolean(id) });
             });
         });
 
-        app.all('/api/*', (req, res) => {
+        app.all('/apiMsg/*', (req, res) => {
             res.json({ error: 404, message: 'Unknown route' });
         });
         
